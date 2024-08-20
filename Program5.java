f88ad22e9ffb5c4c0e5205f453811c1bf4d6d4ae
ellarahn <ellarahn040@gmail.com>
2024-08-20 09:35:52 -0700

    Update Program5.java
    Used System.out.format("%.1f" , variable); to round number to nearest tenth. Looked up how to do it and that's what I found.

--- Program5.java
+++ Program5.java
@@ -1,9 +1,29 @@
-//import stuff here?
-
-//Your code here
-
-//Paste console output below:
-/*
-
-
 */
+class myClass{
+  public static void main(String[]args){
+        
+    
+    
+    double royaleMiles = 286.0;
+    double royaleGal = 9.0;
+    double koopMiles = 412.0;
+    double koopGal = 40.0;
+    double pipeMiles = 361.0;
+    double pipeGal = 18.0;
+    double badMiles = 161.0;
+    double badGal = 11.0;
+    
+    
+    
+    double royalAve = (royaleMiles / royaleGal);
+    double koopAve = koopMiles / koopGal;
+    double pipeAve = pipeMiles / pipeGal;
+    double badAve = badMiles / badGal;
+    
+    System.out.format("%.1f", royalAve);
+    System.out.println("");
+    System.out.format("%.1f", koopAve);
+    System.out.println(" ");
+    System.out.format("%.1f", pipeAve);
+    System.out.println(" ");
+    System.out.format("%.1f", badAve);
